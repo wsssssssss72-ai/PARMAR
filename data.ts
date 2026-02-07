e7f14b3694996055c5e66757e1be9ff99f60fe16
wsssssssss72-ai <wsssssssss72@gmail.com>
2026-02-07 18:25:29 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -27,122 +27,120 @@ export const data: AppData = {
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/94?hash=AgADkR",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v3",
               title: "PREAMBLE",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/96?hash=AgADlx",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v4",
               title: "PART 1 AND 2 OF CONSTITUTION.",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/97?hash=AgADnB",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v5",
               title: "FUNDAMENTAL RIGHTS",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/116?hash=AgADcQ",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v6",
               title: "FUNDAMENTAL RIGHTS ESHA PART ESHA2",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/127?hash=AgAD5g",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v7",
               title: "DPSP AND FUNDAMENTAL DUTIES",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/126?hash=AgADZw",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v8",
               title: "PRESIDENT AND VICEESHAPRESIDENT",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/130?hash=AgAD5Q",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v9",
               title: " PRESIDENT AND VICE-PRESIDENT",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/132?hash=AgADQw",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v10",
               title: "PARLIAMENT - PART -1",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/134?hash=AgADiQ",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v11",
               title: "PARLIAMENT - PART - 2",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/135?hash=AgADTg",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v12",
               title: " STATE LEGISLATURE",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/140?hash=AgADqQ",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
+            },
             {
               id: "v13",
               title: "COURTS ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/136?hash=AgADQQ",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v14",
               title: "LOCAL BODIES ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/144?hash=AgAD3A",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v15",
               title: "LOCAL BODIES - MCQs ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/146?hash=AgAD1g",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-
-             {
+            },
+            {
               id: "v16",
               title: "CONSTITUTIONAL AND NON-CONSTITUTIONAL BODIES ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/142?hash=AgAD4A",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v17",
               title: "EMERGENCY, AMENDMENTS & INTER STATE RELATIONS ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/148?hash=AgAD5Q",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
-              id: "18",
+            },
+            {
+              id: "v18",
               title: "HISTORICAL BACKGROUND (ACTS) ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/150?hash=AgAD1Y",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
             }
           ]
-
         },
         {
           id: "subj-tailwind",
@@ -150,144 +148,140 @@ export const data: AppData = {
           description: ".",
           icon: "",
           videos: [
-           
-             {
+            {
               id: "v1",
               title: "SOLAR SYSTEM ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/153?hash=AgADph",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v2",
               title: "LATITUDE AND LONGITUDE",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/158?hash=AgAD4B",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v3",
               title: "EARTH'S INTERIOR AND PLATE TECHTONICS",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/152?hash=AgADVR",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v4",
               title: "OCEAN AND CONTINENTS ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/156?hash=AgADwx",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v6",
               title: "ROCKS AND VOLCANO",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/162?hash=AgAD-h",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-            
-             {
+            },
+            {
               id: "v7",
               title: "GEOMORPHIC PROCESSES",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/160?hash=AgADJx",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v8",
               title: " LANDFORMS ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/166?hash=AgADVx",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v9",
               title: "ATMOSPHERE ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/164?hash=AgADhx",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v10",
               title: "CONDENSATION AND PRECIPITATION ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/168?hash=AgADAw",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v11",
               title: " WIND",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/172?hash=AgAD3g",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-            
-             {
+            },
+            {
               id: "v12",
               title: "CYCLONE AND OCEAN CURRENTS",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/174?hash=AgAD1A",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v13",
               title: "INDIA AND IT'S LOCATION ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/169?hash=AgADxQ",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
-              id: "14",
+            },
+            {
+              id: "v14",
               title: "HIMALAYAS ",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/180?hash=AgADZg",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v15",
               title: "PENINSULAR PLATEAU",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/178?hash=AgAD3w",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v16",
               title: "PLAINS AND ISLANDS",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/176?hash=AgADaA",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-            
-             {
+            },
+            {
               id: "v17",
               title: "IMALAYAN RIVER SYSTEM",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/182?hash=AgAD3A",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v18",
               title: "RIVER SYSTEM MCQs",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/184?hash=AgADiw",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v19",
               title: "PENINSULAR RIVERS",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/188?hash=AgAD5w",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v20",
               title: "DAMS , LAKES AND WATERFALLS",
               duration: "",
               url: "https://web-production-50ad7.up.railway.app/185?hash=AgADgQ",
               thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2025-09-29-0.06895000379516425.jpg"
-            }
-             {
+            },
+            {
               id: "v21",
               title: "MONSOON",
               duration: "",
@@ -323,4 +317,3 @@ export const data: AppData = {
     }
   ]
 };
-
